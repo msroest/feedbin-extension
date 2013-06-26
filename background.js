bab448cd4885f7566e70fc8516ed95e1824f4560
Mike Roest <msroest@gmail.com>
2013-06-25 21:27:35 -0600

    Trying to update JS to no add an additional timout on every reload.

--- background.js
+++ background.js
@@ -1,15 +1,20 @@
 function registerReload(tabId,changeInfo,tab) {
 
 	if(tab.url == "https://feedbin.me/" || tab.url == "https://feedbin.me") {
-		var refresh = 0+localStorage["refresh_interval"];
+		var refresh = localStorage["refresh_interval"];
 		if(!refresh)
 			refresh = 300;
 		refresh = refresh*1000;
 		
-		setTimeout(function() { chrome.tabs.reload(tabId);},refresh)
+		setTimeout(function() {doReload(tabId);},refresh)
 	}
 
 }
+function doReload(tabId) {
+	chrome.tabs.reload(tabId);
+	return;
+}
+
 
 chrome.tabs.onUpdated.addListener(registerReload);
 
